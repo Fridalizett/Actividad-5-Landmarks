a3818fff83f1ff62723f180dd5361a1dd33a0164
Fridalizett <100945720+Fridalizett@users.noreply.github.com>
2023-05-31 23:22:53 -0600

    Update ejemplo_mrsDiffDrivePurePursuit.m

--- ejemplo_mrsDiffDrivePurePursuit.m
+++ ejemplo_mrsDiffDrivePurePursuit.m
@@ -12,15 +12,29 @@ dd = DifferentialDrive(R,L);
 
 %% Simulation parameters
 sampleTime = 0.1;               % Sample time [s]
-tVec = 0:sampleTime:160;         % Time array
+tVec = 0:sampleTime:80;         % Time array
 
-initPose = [5;1;-2*pi];             % Initial pose (x y theta)
+initPose = [2;2;pi];             % Initial pose (x y theta)
 pose = zeros(3,numel(tVec));    % Pose matrix
 pose(:,1) = initPose;
 
 % Define waypoints
+%waypoints = [0,0; 2,2; 4,2; 2,4; 0.5,3];
+
+%waypoints1 = [2,2; -5,4; 4,-0.5; -3.5,-3; 0,2.5; -1.5,0; 1,-2.5; 0,0];
+
+%waypoints2 = [2,5; -5,3; -5,-2; 2,-5; 5,2; -3,2; -4,-4; 4,-3];
+
+%waypoints3 = [-3,4; 3,3; 1,-3; -1,-1; 1,4; -3,-4; 2,-1];
+
 %perro
 waypoints = [-4,3; -5,2; -5,3; -3,3; -3,4; -1,4; -2,3; -2,4.3; 0,5; -1,6; -1,4.8; 0,5; 1,5; 1,4; 1,6; 2,3; 1,2; 2,3; 1,6; 3,2; 5,0; 0,-2; 0,-1; 4,1; 0,-1; -1,0; -4,0; -3,1; -2,1; -3,1; -4,0; -5,2];
+
+%flor
+%waypoints = [4,1; 2,1; 0,3; 2,3; 4,1; 6,1; 8,3; 6,3; 4,1; 4,5; 2,3; 2,5; 0,5; 2,7; 0,9; 2,9; 2,11; 4,9; 6,11; 6,9; 8,9; 6,7; 8,5; 6,5; 6,3; 3,6; 3,8; 5,8; 5,6; 3,6];
+
+%cereza
+%waypoints = [5,1; 4,2; 3,3; 3,4; 3,5; 4,6; 5,7; 6,7; 7,7 ; 8,6; 7,5; 6,6; 7,5; 8,5; 7,5; 8,6; 10,9; 9,10; 8,11; 6,11; 5,10; 4,9; 10,9; 8,6; 9,5; 9,3; 8,2; 7,1; 5,1];
 
 
 % Create visualizer
@@ -30,9 +44,9 @@ viz.hasWaypoints = true;
 %% Pure Pursuit Controller
 controller = controllerPurePursuit;
 controller.Waypoints = waypoints;
-controller.LookaheadDistance = 0.2;
-controller.DesiredLinearVelocity = 0.3;
-controller.MaxAngularVelocity = 25;
+controller.LookaheadDistance = 0.35;
+controller.DesiredLinearVelocity = 0.75;
+controller.MaxAngularVelocity = 1.5;
 
 %% Simulation loop
 close all
